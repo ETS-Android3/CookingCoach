125021a74c089d83ab9cadf2c6800de144086c0a
Sakchi123 <sakchi.shrestha@mavs.uta.edu>
2022-04-02 20:55:34 -0500

    Last update!!

--- app/src/main/java/com/example/CookingCoach/EditProfile.java
+++ app/src/main/java/com/example/CookingCoach/EditProfile.java
@@ -3,9 +3,15 @@ package com.example.CookingCoach;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditProfile extends AppCompatActivity {
 
+    private TextView fullName;
+    private EditText editName, editEmail, editAge;
+    private Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
