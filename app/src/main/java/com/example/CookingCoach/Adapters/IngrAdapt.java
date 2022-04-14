436278a65097b375593c1f6c75de2391693f5ad8
devnaganoolil <devnaganoolil@gmail.com>
2022-04-13 20:05:40 -0500

    Recipe Details

---
+++ app/src/main/java/com/example/CookingCoach/Adapters/IngrAdapt.java
@@ -0,0 +1,63 @@
+package com.example.CookingCoach.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.CookingCoach.Models.ExtendedIngredient;
+import com.example.CookingCoach.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class IngrAdapt extends RecyclerView.Adapter<ingrHolder>{
+
+    Context info;
+    List<ExtendedIngredient> list;
+    //make a constructor for both of them to get the info
+
+    public IngrAdapt(Context info, List<ExtendedIngredient> list) {
+        this.info = info;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ingrHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ingrHolder(LayoutInflater.from(info).inflate(R.layout.meal_ingredients, parent,false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ingrHolder holder, int position) {
+        //get info for text name and quantity from the list
+        holder.ingreName.setText(list.get(position).name);
+        //holder.ingreName.setSelected(true);
+        holder.ingrQuantity.setText(list.get(position).original);
+        //holder.ingrQuantity.setSelected(true);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
+class ingrHolder extends RecyclerView.ViewHolder
+{
+    TextView ingrQuantity, ingreName;
+    //ImageView ingrImage;
+
+    public ingrHolder(@NonNull View itemView) {
+        super(itemView);
+
+        //initialize the ids
+        ingrQuantity = itemView.findViewById(R.id.ingrQuantity);
+        ingreName = itemView.findViewById(R.id.ingreName);
+        //ingrImage = itemView.findViewById(R.id.ingrImage);
+    }
+}
--- app/src/main/java/com/example/CookingCoach/RecipieDetActivity.java
+++ app/src/main/java/com/example/CookingCoach/RecipieDetActivity.java
@@ -1,14 +1,86 @@
 package com.example.CookingCoach;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.CookingCoach.Adapters.IngrAdapt;
+import com.example.CookingCoach.Listeners.RecipeDetListener;
+import com.example.CookingCoach.Models.RecipeDetRes;
+import com.squareup.picasso.Picasso;
 
 public class RecipieDetActivity extends AppCompatActivity {
+    int id;
+    TextView mealName;
+    ImageView mealImage;
+    RecyclerView mealIngredients;
+    RequestManager requestmana;
+    ProgressDialog progressdia;
+    IngrAdapt adapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipie_det);
+
+        //create the the way to call the variables in the layout xml
+        findIds();
+
+        //get the id from the intent
+        id = Integer.parseInt(getIntent().getStringExtra("id"));
+
+        //initialize the request manager
+        requestmana = new RequestManager(this);
+
+        //call the api to get details
+        requestmana.getRecipeDet(recipeDetlisten,id);
+
+        //set up dialog
+        //just a waiting screen (can be removed, not really needed)
+        progressdia = new ProgressDialog(this);
+        progressdia.setTitle("Loading Details...");
+        progressdia.show();
     }
+
+    private void findIds() {
+        //initialize the ids
+        mealName = findViewById(R.id.mealName);
+        //mealSource = findViewById(R.id.mealSource);
+        mealImage = findViewById(R.id.mealImage);
+        mealIngredients = findViewById(R.id.mealIngredients);
+    }
+
+    //create the recipe detail listener object
+    private final RecipeDetListener recipeDetlisten = new RecipeDetListener() {
+        @Override
+        public void didFetch(RecipeDetRes response, String message) {
+            progressdia.dismiss();
+
+            //set the meal name from the response
+            mealName.setText(response.title);
+
+            //set the meal image
+            Picasso.get().load(response.image).into(mealImage);
+
+            //set the ingredient
+            mealIngredients.setHasFixedSize(true);
+            mealIngredients.setLayoutManager(new LinearLayoutManager(RecipieDetActivity.this,LinearLayoutManager.VERTICAL,false));
+
+            //set the adapter
+            adapt = new IngrAdapt(RecipieDetActivity.this,response.extendedIngredients);
+            mealIngredients.setAdapter(adapt);
+        }
+
+        @Override
+        public void didError(String message) {
+            //show a message with a error
+            Toast.makeText(RecipieDetActivity.this,message,Toast.LENGTH_SHORT).show();
+        }
+    };
 }
