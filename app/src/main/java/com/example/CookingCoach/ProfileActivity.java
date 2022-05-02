7d80645c5d014d4b03bfc30d72db24aa944b1430
haziq462 <haziqzuhaimi@ymail.com>
2022-05-01 23:28:23 -0500

    Just added some text for profileactivity. Feel free to edit if you want something else

--- app/src/main/java/com/example/CookingCoach/ProfileActivity.java
+++ app/src/main/java/com/example/CookingCoach/ProfileActivity.java
@@ -1,5 +1,6 @@
 package com.example.CookingCoach;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,12 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import android.content.pm.PackageManager;
 import android.media.ThumbnailUtils;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -20,8 +25,10 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Bitmap;
+import java.io.InputStream;
 
 
 import com.example.CookingCoach.Adapters.RandomRecipeAdapter;
@@ -30,7 +37,12 @@ import com.example.CookingCoach.Listeners.RecipeClick;
 import com.example.CookingCoach.Models.RandomRecipieApiResponse;
 import com.example.CookingCoach.ml.MobilenetV110224Quant;
 import com.google.firebase.auth.FirebaseAuth;
-
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import org.tensorflow.lite.DataType;
 import org.tensorflow.lite.support.model.Model;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
@@ -43,117 +55,83 @@ import java.util.List;
 
 public class ProfileActivity extends AppCompatActivity {
     ProgressDialog dialog;
+    Bitmap bitmap;
     RequestManager manager;
     RandomRecipeAdapter randomRecipeAdapter;
     RecyclerView recyclerView;
     SearchView searchView;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userId;
     List<String> tags = new ArrayList<>();
-    ImageButton camera;
-    int imageSize = 32;
+
 
       @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_profile);
+          dialog = new ProgressDialog(this);
+          dialog.setTitle("Loading...");
 
-        camera = findViewById(R.id.imageButton);
-        camera.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View view) {
-                if((checkSelfPermission(Manifest.permission.CAMERA)) == PackageManager.PERMISSION_GRANTED){
-                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(cameraIntent,3);
-                }else{
-                    requestPermissions(new String[]{Manifest.permission.CAMERA},100);
-                }
+          //initialize the search view
+          searchView = findViewById(R.id.searchView);
 
-            }
-        });
+          //create a text listener for the search view
+          searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+              @Override
+              public boolean onQueryTextSubmit(String query) {
+                  //clear all of the tags first
+                  tags.clear();
+                  //then add the search to the tags
+                  tags.add(query.toString());
+                  //then search the tag made
+                  manager.getRandomRecipies(randomRecipeResponseListener, tags);
+                  //then show th results
+                  dialog.show();
 
+                  return true;
+              }
 
-        dialog = new ProgressDialog(this);
-        dialog.setTitle("Loading...");
-
-        //initialize the search view
-        searchView = findViewById(R.id.searchView);
-
-        //create a text listener for the search view
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                //clear all of the tags first
-                tags.clear();
-                //then add the search to the tags
-                tags.add(query.toString());
-                //then search the tag made
-                manager.getRandomRecipies(randomRecipeResponseListener, tags);
-                //then show th results
-                dialog.show();
-
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
+              @Override
+              public boolean onQueryTextChange(String newText) {
+                  return false;
+              }
 
 
-        });
+          });
 
-        manager = new RequestManager(this);
-       // manager.getRandomRecipies(randomRecipeResponseListener);
-        //dialog.show();
-    }
-    public void classifyImage(Bitmap image){
-        try {
-            MobilenetV110224Quant model = MobilenetV110224Quant.newInstance(getApplicationContext());
+          user =  FirebaseAuth.getInstance().getCurrentUser();
+          reference = FirebaseDatabase.getInstance().getReference("Users");
+          userId = user.getUid();
+          final TextView greetingTextView = (TextView) findViewById(R.id.greeting);
 
-            // Creates inputs for reference.
-            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
-            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
-            byteBuffer.order(ByteOrder.nativeOrder());
+          reference.child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+              @Override
+              public void onDataChange(@NonNull DataSnapshot snapshot) {
+                  User userProfile = snapshot.getValue(User.class);
 
-            // get 1D array of 224 * 224 pixels in image
-            int [] intValues = new int[imageSize * imageSize];
-            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+                  if (userProfile != null)
+                  {
+                      String name = userProfile.fullName;
 
-            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
-            int pixel = 0;
-            for(int i = 0; i < imageSize; i++){
-                for(int j = 0; j < imageSize; j++){
-                    int val = intValues[pixel++]; // RGB
-                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
-                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
-                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
-                }
-            }
+                      greetingTextView.setText("  Enter any ingredient " + name + " !");
+                  }
 
-            inputFeature0.loadBuffer(byteBuffer);
+              }
 
-            // Runs model inference and gets result.
-            MobilenetV110224Quant.Outputs outputs = model.process(inputFeature0);
-            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+              @Override
+              public void onCancelled(@NonNull DatabaseError error) {
+                  Toast.makeText(ProfileActivity.this, "Error getting the data!", Toast.LENGTH_LONG).show();
 
-            float[] confidences = outputFeature0.getFloatArray();
-            // find the index of the class with the biggest confidence.
-            int maxPos = 0;
-            float maxConfidence = 0;
-            for(int i = 0; i < confidences.length; i++){
-                if(confidences[i] > maxConfidence){
-                    maxConfidence = confidences[i];
-                    maxPos = i;
-                }
-            }
+              }
+          });
+
+          manager = new RequestManager(this);
+          // manager.getRandomRecipies(randomRecipeResponseListener);
+          //dialog.show();
+       }
 
 
-            // Releases model resources if no longer used.
-            model.close();
-        } catch (IOException e) {
-            // TODO Handle the exception
-        }
-    }
 
     private final RandomRecipeResponseListener randomRecipeResponseListener = new RandomRecipeResponseListener() {
         @Override
@@ -186,20 +164,5 @@ public class ProfileActivity extends AppCompatActivity {
         }
     };
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data)
-    {
-        if(resultCode == RESULT_OK)
-        {
-            if(resultCode == 3)
-            {
-                Bitmap image = (Bitmap) data.getExtras().get("data");
-                int dimension = Math.min(image.getWidth(), image.getHeight());
-                image = ThumbnailUtils.extractThumbnail(image, dimension, dimension);
-                classifyImage(image);
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
 
-    }
 }
